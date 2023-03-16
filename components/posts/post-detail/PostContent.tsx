24931ba92c1195639072d95614c0db67233982ae
dev-thug <de0978@gmail.com>
2023-03-16 19:24:39 +0900

    feat: Add a post detail

---
+++ components/posts/post-detail/PostContent.tsx
@@ -0,0 +1,22 @@
+import classes from './post-content.module.css';
+import PostHeader from './PostHeader';
+
+const DUMMY_POST = {
+  slug: 'getting-started-with-nextjs4',
+  title: 'Getting Started with Nextjs',
+  image: 'getting-started-with-nextjs.png',
+  date: '2023-03-16',
+  content: '# This is a first post'
+};
+
+const PostContent = () => {
+  const imagePsth = `/images/posts/${DUMMY_POST.image}`;
+  return (
+    <article className={classes.content}>
+      <PostHeader title={DUMMY_POST.title} image={imagePsth} />
+      {DUMMY_POST.content}
+    </article>
+  );
+};
+
+export default PostContent;
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,5 +1,7 @@
+import PostContent from '@/components/posts/post-detail/PostContent';
+
 const PostDetailPage = () => {
-  return <></>;
+  return <PostContent />;
 };
 
 export default PostDetailPage;
